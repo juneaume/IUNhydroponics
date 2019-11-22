56abc11c6dbf2a71203388c43bbb5b2abd6c5945
juneaume <58079340+juneaume@users.noreply.github.com>
2019-11-22 13:04:57 -0600

    Update pHcode.py

--- pHcode.py
+++ pHcode.py
@@ -2,7 +2,6 @@
 
 import io
 import fcntl
-import datetime
 import time
 import string
 from atlas_i2c import atlas_i2c
@@ -13,9 +12,8 @@ def main():
 	# creates the I2C port object,specify the address or bus if necessary
 	print(">> Continuously polls the board every xx.x seconds,")
 	print(" where xx.x is longer than the {} second timeout.".format(atlas_i2c.long_timeout))
-	print(" Pressing ctrl-c will stop the polling")
+	print(" Pressing Ctrl-C will stop the polling.")
 
-	# main loop
 	while True:
 		polltime = float(input("Enter poll time: "))
 		if(polltime < atlas_i2c.long_timeout):
@@ -24,15 +22,11 @@ def main():
 
 		try:
 			while True:
-				timestamp = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-				value = device.query("R")
-				print(timestamp + "\t pH: " + value)
+				print(device.query("R"))
 				time.sleep(polltime - atlas_i2c.long_timeout)
 		except KeyboardInterrupt():
-			# catches the ctrl-c command, which breaks the loop above
 			print("Continuous polling stopped.")
 
-	# if not a special keyword, pass commands straight to board
 	else:
 		try:
 			print(device.query(polltime))
