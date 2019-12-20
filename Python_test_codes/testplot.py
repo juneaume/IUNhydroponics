41f1f47b6e27b261b4ef632c4d96db3b3afedd0c
juneaume <revelationtordue@gmail.com>
2019-12-20 10:56:11 -0600

    20/12/19

--- Python_test_codes/testplot.py
+++ Python_test_codes/testplot.py
@@ -5,7 +5,7 @@ import pandas as pd
 from tkinter.filedialog import askopenfilename
 
 file = pd.read_csv(askopenfilename(), sep=',', index_col=1)
-#file = pd.read_csv("/home/pi/2019-11-27_results.csv", sep=',', index_col=1)
+#file = pd.read_csv("/home/pi/TempHum_Results/2019-12-19_results.csv", sep=',', index_col=1)
 
 df = pd.DataFrame(file)
 df.set_axis(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'], axis='columns', inplace=True)
@@ -26,23 +26,55 @@ allh = twoh.append(s2h, ignore_index=True)
 atemps = df['B']
 ahums = df['C']
 
+def xtickval(value):
+	toUse = []
+	for i in range(len(value)):
+		if len(value) < 300:
+			if (i % 10 == 0) == True:
+				toUse.append(df.index[i])
+		elif len(value) > 300:
+			if (i % 100 == 0) == True:
+				toUse.append(df.index[i])
+	return toUse
+
 plt.figure()
 
-plt.subplot(211)
-plt.plot(times, atemps)
+ax1 = plt.subplot(211)
+ax1.plot(times, atemps, color="green")
 plt.xlim(min(times), max(times))
-plt.ylim(min(allt), max(allt))
-plt.xticks([])
-plt.xlabel('Time between' + str(min(times)) + 'and' + str(max(times)))
+#plt.ylim(min(allt), max(allt))
 plt.ylabel('Temperature (*C)')
+plt.xticks(xtickval(df.index))
+plt.setp(ax1.get_xticklabels(), rotation = -25, ha = "left")
 
-plt.subplot(212)
-plt.plot(times, ahums)
+offset = -72
+bbox = dict(boxstyle="round", fc="0.8")
+arrowprops = dict(arrowstyle = "->", connectionstyle = "angle,angleA=0,angleB=90,rad=10")
+
+ax1.annotate((
+	"Maximum " + str(atemps.max()) + '*C at' + str(atemps.idxmax())),
+	xy=(atemps.idxmax(), atemps.max()),
+	xytext=(offset, 2.5*offset), textcoords='offset points',
+	bbox=bbox, arrowprops = arrowprops)
+
+plt.grid(True)
+
+ax2 = plt.subplot(212)
+ax2.plot(times, ahums, color="purple")
 plt.xlim(min(times), max(times))
-plt.ylim(min(allh), max(allh))
-plt.xticks([])
+#plt.ylim(min(allh), max(allh))
 plt.xlabel('Time between' + str(min(times)) + 'and' + str(max(times)))
 plt.ylabel('Humidity (%)')
+plt.xticks(xtickval(df.index))
+plt.setp(ax2.get_xticklabels(), rotation = -25, ha = "left")
+
+ax2.annotate((
+	"Maximum " + str(ahums.max()) + '% at' + str(ahums.idxmax())),
+	xy=(ahums.idxmax(), ahums.max()),
+	xytext=(offset, 2.5*offset), textcoords='offset points',
+	bbox = bbox, arrowprops = arrowprops)
+
+plt.grid(True)
 
 plt.suptitle("Temperature and Humidity for " + df.iloc[1]['A'])
 
